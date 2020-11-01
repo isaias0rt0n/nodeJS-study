9b05b0e01fe7dfe7c49db492b9c8609fe066f0cf
unknown <isaiasorton@gmail.com>
2020-11-01 08:36:05 -0300

    criado projeto index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const express = requeri('express');
+const { request, response } = require('express');
+const express = require('express');
 
 const app = express();
+
+app.get('/project', (request, response) => {
+  return response.send('ola');
+});
+
+app.listen(3333);
